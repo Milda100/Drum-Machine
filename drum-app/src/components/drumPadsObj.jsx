0eb63390ca3f91d218f3aa280cad06bab10ec8c0
Milda100 <157160973+Milda100@users.noreply.github.com>
2025-05-05 20:47:02 +0300

    little fix

--- drum-app/src/components/drumPadsObj.jsx
+++ drum-app/src/components/drumPadsObj.jsx
@@ -5,8 +5,8 @@ const drumPads = [
     { id: "Heater-4", key: "A", src: "https://cdn.freecodecamp.org/testable-projects-fcc/audio/Heater-4_1.mp3" },
     { id: "Heater-6", key: "S", src: "https://cdn.freecodecamp.org/testable-projects-fcc/audio/Heater-6.mp3" },
     { id: "Dsc_Oh", key: "D", src: "https://cdn.freecodecamp.org/testable-projects-fcc/audio/Dsc_Oh.mp3" },
-    { id: "Kick_n_Hat", key: "Z", src: "https://cdn.freecodecamp.org/testable-projects-fcc/audio/Kick_n_Hat.mp3" },
-    { id: "RP4_KICK_1", key: "X", src: "https://cdn.freecodecamp.org/testable-projects-fcc/audio/RP4_KICK_1.mp3" },
+    { id: "Kick", key: "Z", src: "https://cdn.freecodecamp.org/testable-projects-fcc/audio/Kick_n_Hat.mp3" },
+    { id: "RP4_KICK", key: "X", src: "https://cdn.freecodecamp.org/testable-projects-fcc/audio/RP4_KICK_1.mp3" },
     { id: "Cev_H2", key: "C", src: "https://cdn.freecodecamp.org/testable-projects-fcc/audio/Cev_H2.mp3" }
   ];
 
